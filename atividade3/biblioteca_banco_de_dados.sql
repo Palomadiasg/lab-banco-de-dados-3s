1a5d489bfad54409c30b1cb42f6a2fd21968ca4a
Palomadiasg <palomadias2310@gmail.com>
2026-03-25 19:58:51 -0300

    Delete atividade3/biblioteca_banco_de_dados.sql

--- atividade3/biblioteca_banco_de_dados.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Usuario (
-
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    data_cadastro DATE NOT NULL
-    
-    );
-    SHOW TABLES
