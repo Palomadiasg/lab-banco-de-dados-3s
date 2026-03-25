706ce2af2fa010fb581d560f3c3b8d60d6ceec11
Palomadiasg <palomadias2310@gmail.com>
2026-03-25 19:58:17 -0300

    Add files via upload

---
+++ atividade3/script_bd_biblioteca_pessoal_v1.sql
@@ -0,0 +1,64 @@
+DROP DATABASE IF EXISTS biblioteca_pessoal_2026;
+CREATE DATABASE biblioteca_pessoal_2026;
+USE biblioteca_pessoal_2026;
+
+CREATE TABLE usuario (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(128) NOT NULL UNIQUE,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE autor (
+    id_autor INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL UNIQUE,
+    ano_nascimento YEAR,
+    ano_morte YEAR,
+    apresentacao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categoria (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(128) NOT NULL,
+    descricao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE editora (
+    id_editora INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    cidade VARCHAR(64),
+    estado VARCHAR(64),
+    pais VARCHAR(64),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE livro (
+    id_livro INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    sinopse TEXT,
+    ano_publicacao YEAR,
+    id_usuario INT NOT NULL,
+    id_autor INT NOT NULL,
+    id_editora INT NOT NULL,
+    id_categoria INT NOT NULL,
+
+    
+    CONSTRAINT fk_livro_usuario FOREIGN KEY (id_usuario) 
+        REFERENCES usuario(id_usuario) ON DELETE CASCADE,
+        
+    CONSTRAINT fk_livro_autor FOREIGN KEY (id_autor) 
+        REFERENCES autor(id_autor) ON DELETE RESTRICT,
+        
+    CONSTRAINT fk_livro_editora FOREIGN KEY (id_editora) 
+        REFERENCES editora(id_editora) ON DELETE RESTRICT,
+        
+    CONSTRAINT fk_livro_categoria FOREIGN KEY (id_categoria) 
+        REFERENCES categoria(id_categoria) ON DELETE RESTRICT
+);
