0758f9eb41f7d5c887ba7464e47da018da8da003
Palomadiasg <palomadias2310@gmail.com>
2026-04-08 20:39:12 -0300

    script_bd_biblioteca_pessoal_v1

--- atividade3/script_bd_biblioteca_pessoal_v1.sql
+++ atividade3/script_bd_biblioteca_pessoal_v1.sql
@@ -12,6 +12,7 @@ CREATE TABLE usuario (
     data_atualizacao TIMESTAMP
 );
 
+
 CREATE TABLE autor (
     id_autor INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL UNIQUE, 
@@ -21,6 +22,7 @@ CREATE TABLE autor (
     data_cadastro TIMESTAMP,
     data_atualizacao TIMESTAMP
 );
+
 
 CREATE TABLE editora (
     id_editora INT AUTO_INCREMENT PRIMARY KEY,
@@ -59,38 +61,4 @@ CREATE TABLE livro (
     FOREIGN KEY (id_autor) REFERENCES autor(id_autor),
     FOREIGN KEY (id_editora) REFERENCES editora(id_editora),
     FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
-);
-
-CREATE TABLE editora (
-    id_editora INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    cidade VARCHAR(64),
-    estado VARCHAR(64),
-    pais VARCHAR(64),
-    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
-CREATE TABLE livro (
-    id_livro INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    sinopse TEXT,
-    ano_publicacao YEAR,
-    id_usuario INT NOT NULL,
-    id_autor INT NOT NULL,
-    id_editora INT NOT NULL,
-    id_categoria INT NOT NULL,
-
-    
-    CONSTRAINT fk_livro_usuario FOREIGN KEY (id_usuario) 
-        REFERENCES usuario(id_usuario) ON DELETE CASCADE,
-        
-    CONSTRAINT fk_livro_autor FOREIGN KEY (id_autor) 
-        REFERENCES autor(id_autor) ON DELETE RESTRICT,
-        
-    CONSTRAINT fk_livro_editora FOREIGN KEY (id_editora) 
-        REFERENCES editora(id_editora) ON DELETE RESTRICT,
-        
-    CONSTRAINT fk_livro_categoria FOREIGN KEY (id_categoria) 
-        REFERENCES categoria(id_categoria) ON DELETE RESTRICT
 );
